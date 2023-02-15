095f816ec54d2f5e7779a061cc533021b59b4eeb
bachacon <brandynchacon@gmail.com>
2023-02-14 19:27:30 -0700

    Big Mac Index Code

--- code_4.py
+++ code_4.py
@@ -15,7 +15,7 @@ def get_big_mac_price_by_year(year,country_code):
             big_mac_price += float(data[6])
     mean_big_mac_price = big_mac_price / float(line_counter)
     
-
+    
     return round(mean_big_mac_price, 2)
 
 
@@ -32,7 +32,7 @@ def get_big_mac_price_by_country(country_code):
             big_mac_price += float(data[6])
     mean_big_mac_price = big_mac_price / float(line_counter)
     
-
+    
     return round(mean_big_mac_price, 2)
 
 def get_the_cheapest_big_mac_price_by_year(year):
@@ -48,8 +48,8 @@ def get_the_cheapest_big_mac_price_by_year(year):
             lowest_price = round(lowest_price, 2)
             country_name = data[3]
             code = data[1]
-
-
+    
+    
     return f"{country_name}({code}): ${lowest_price}"
 
 
@@ -66,17 +66,17 @@ def get_the_most_expensive_big_mac_price_by_year(year):
             highest_price = round(highest_price, 2)
             country_name = data[3]
             code = data[1]
-
-
+    
+    
     return f"{country_name}({code}): ${highest_price}"
 
 if __name__ == "__main__":
     print("\nWelcome to the Big Mac Index Program!")
-
+    
     print("\nYou will be asked to indicate what you need: (Big Mac Price by Year), (Big Mac Price by Country),\
         (Cheapest Big Mac Price by Year), and (Most Expensive Big Mac Price by Year). Type (exit) if you no longer need the program")
-
-
+    
+    
     while True:
         user_decide = input("\nWhat would you like to know? \n")
         if user_decide.lower() == "big mac price by year":
@@ -96,6 +96,7 @@ if __name__ == "__main__":
             break
         else:
             print("\nSomething was typed incorrectly please try again!")
+
+
+
             
-
-
